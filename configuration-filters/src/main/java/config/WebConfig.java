fbdc62101ae9706a3f234b6183228ae3c76b5b34
seowonn <sonseowon5@gmail.com>
2025-01-16 14:24:47 +0900

    Chore: WebMvcConfigurer 오버라이드 메서드 정리

--- configuration-filters/src/main/java/config/WebConfig.java
+++ configuration-filters/src/main/java/config/WebConfig.java
@@ -6,14 +6,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * WebConfig 역할: MVC 설정과 정적 resource handling을 정의한다.
+ */
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages={"controller"})
+@ComponentScan(basePackages = { "controller" })
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/assets/**")
-                .addResourceLocations("classpath:/assets/");
-    }
+
+	// 정적 파일 요청을 처리하기 위한 리소스 핸들러 등록
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**") // 클라이언트가 요청하는 경로
+				.addResourceLocations("classpath:/assets/"); // 정적 리소스 파일의 실제 위치
+	}
 }
